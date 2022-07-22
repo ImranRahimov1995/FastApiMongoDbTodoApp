f5bb0a2c3e6f9949786122df7be2f5dcb6f8a7bd
ImranRahimov1995 <imash.odessahouse@gmail.com>
2022-07-22 22:42:41 +0400

    Extend app ,optimize. Add config, add func on event "startup" which create models in db

--- src/app.py
+++ src/app.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
 
-app = FastAPI()
+from src.core.config import settings
+from src.models.user_model import User
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+)
 
 
-@app.get('/')
-async def hello():
-    return {"message": "Simple app working"}
+@app.on_event("startup")
+async def app_init():
+    """
+        initialize crucial application services
+    """
 
+    # init db client , fodoist is app name nothing else
+    db_client = AsyncIOMotorClient(settings.MONGO_CONNECTION_STRING).fodoist
+
+    await  init_beanie(
+        database=db_client,
+        document_models=[
+            User,
+        ]
+    )
 
 # Run command
 # uvicorn src.app:app --reload
