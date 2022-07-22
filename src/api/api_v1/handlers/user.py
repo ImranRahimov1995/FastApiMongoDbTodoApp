b3736d15b6cb07a97baa0f745f83aa3f1cf10524
ImranRahimov1995 <imash.odessahouse@gmail.com>
2022-07-22 23:44:31 +0400

    ADD user create handler .

--- src/api/api_v1/handlers/user.py
+++ src/api/api_v1/handlers/user.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from pymongo import errors
+
+from src.schemas.user_schema import UserAuth,UserOut
+from src.services.user_service import UserService
 
 user_router = APIRouter()
 
 
-@user_router.get('/test')
-async def test():
-    return {'message', "ok"}
+@user_router.post('/create', summary="Create new user",response_model=UserOut)
+async def create_user(data: UserAuth):
+    try:
+
+        return await UserService.create_user(data)
+    except errors.DuplicateKeyError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='User with this email or username is exists'
+        )
