f38f055bacc16cc7e390ee15ff84c51937b07d34
ImranRahimov1995 <imash.odessahouse@gmail.com>
2022-07-23 21:30:45 +0400

    ADD new methods in UserService
    authenticate
    get_user_by_email
    get_user_by_id

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,3 +1,6 @@
+from typing import Optional
+from uuid import UUID
+
 from src.schemas.user_schema import UserAuth
 from src.models.user_model import User
 from src.core.security import get_password, verify_password
@@ -13,3 +16,27 @@ class UserService:
         )
         await user_in.save()
         return user_in
+
+    @staticmethod
+    async def authenticate(email: str, password: str) -> Optional[User]:
+        user = await UserService.get_user_by_email(email=email)
+
+        if not user:
+            return None
+
+        if not verify_password(password, hashed_pass=user.hashed_password):
+            return None
+
+        return user
+
+    @staticmethod
+    async def get_user_by_email(email: str) -> Optional[User]:
+        user = await User.find_one(User.email == email)
+
+        return user
+
+    @staticmethod
+    async def get_user_by_id(user_id: UUID) -> Optional[User]:
+        user = await User.find_one(User.user_id == user_id)
+
+        return user
