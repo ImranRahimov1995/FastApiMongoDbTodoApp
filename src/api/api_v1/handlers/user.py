df7b05ad4119891a541199be54b35c9ba556548c
ImranRahimov1995 <imash.odessahouse@gmail.com>
2022-07-23 20:32:16 +0400

    CHANGE status code. in create_user handler.

--- src/api/api_v1/handlers/user.py
+++ src/api/api_v1/handlers/user.py
@@ -1,17 +1,22 @@
 from fastapi import APIRouter, HTTPException, status
 from pymongo import errors
 
-from src.schemas.user_schema import UserAuth,UserOut
+from src.schemas.user_schema import UserAuth, UserOut
 from src.services.user_service import UserService
 
 user_router = APIRouter()
 
 
-@user_router.post('/create', summary="Create new user",response_model=UserOut)
+@user_router.post('/create',
+                  summary="Create new user",
+                  response_model=UserOut,
+                  status_code=status.HTTP_201_CREATED)
 async def create_user(data: UserAuth):
     try:
 
-        return await UserService.create_user(data)
+        user = await UserService.create_user(data)
+        return user
+
     except errors.DuplicateKeyError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
