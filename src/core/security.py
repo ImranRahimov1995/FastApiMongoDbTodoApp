4d41cde59fb3cba74c9b0f5ce415f9c0102159e8
ImranRahimov1995 <imash.odessahouse@gmail.com>
2022-07-23 21:29:25 +0400

    ADD jwt creating tokens in security.
    create access token.
    create refresh token.

--- src/core/security.py
+++ src/core/security.py
@@ -1,4 +1,10 @@
+from typing import Union, Any
+from datetime import datetime, timedelta
+
 from passlib.context import CryptContext
+from jose import jwt
+
+from src.core.config import settings
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +15,45 @@ def get_password(password: str) -> str:
 
 def verify_password(password: str, hashed_pass: str) -> bool:
     return password_context.verify(password, hashed_pass)
+
+
+def create_access_token(subject: Union[str, Any],
+                        expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+
+    else:
+        expires_delta = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode = {
+        "exp": expires_delta,
+        "sub": str(subject)
+    }
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
+
+
+def create_refresh_token(subject: Union[str, Any],
+                        expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+
+    else:
+        expires_delta = datetime.utcnow() + timedelta(
+            minutes=settings.REFRESH_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode = {
+        "exp": expires_delta,
+        "sub": str(subject)
+    }
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.JWT_REFRESH_SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
